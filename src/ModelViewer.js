e4d0cc11be5693eee10313d08eed435dcea72e14
Dylan Alvandi <geradylanalvandi@gmail.com>
2022-11-14 23:21:06 +0100

    add meaningful comments, reposition camera

--- src/ModelViewer.js
+++ src/ModelViewer.js
@@ -13,34 +13,40 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 class ModelViewer extends React.Component {
 
-      componentDidMount() {
-    // === THREE.JS CODE START ===
+    componentDidMount() {
+    /*
+    Three JS scene instantiation
+    */
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    //Don't append the 3D renderer to the document itself
-    //document.body.appendChild( renderer.domElement );
 
+    /*
+    Orbital COntrols instantiation
+    */
     const controls = new OrbitControls( camera, renderer.domElement );
 
     //controls.update() must be called after any manual changes to the camera's transform
-    camera.position.set( 0, 20, 100 );
+    camera.position.set( 0, 0, 10);
     controls.update();
 
+    /*
+    Scene setup
+    */
+    //TODO: Check why appendChild is needed here
     this.mount.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
-    camera.position.z = 5;
+    camera.position.z = 3;  //camera distance from the object
     var animate = function () {
       requestAnimationFrame( animate );
       //cube.rotation.y += 0.01;
       renderer.render( scene, camera );
     };
     animate();
-    // === THREE.JS EXAMPLE CODE END ===
   }
 
     render() {
